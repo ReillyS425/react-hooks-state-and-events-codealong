619cab965e4591c35d694bb4462304e13e54a55e
Reilly Shofner <reilly.shofner@gmail.com>
2025-05-07 15:12:09 -0700

    complete react-hooks-state-and-events-codealong

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  function handClick() {
+    setIsOn((isOn) => !isOn);
+  }
+const color = isOn ? "red" : "white";
+
+  return <button style={{ background: color }} onClick={handClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
